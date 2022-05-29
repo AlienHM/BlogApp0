214d962fa9f97a8d380245760563ff6ad8443f8a
AlienHM <alihesenov15@gmail.comm>
2022-05-29 17:13:55 +0300

    bootstrapify change password

--- members/views.py
+++ members/views.py
@@ -4,11 +4,11 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import EditProfileForm, SignUpForm
+from .forms import EditProfileForm, SignUpForm, PasswordChangingForm
 # Create your views here.
 
 class PasswordsChangeView(PasswordChangeView):
-    form_class = PasswordChangeForm
+    form_class = PasswordChangingForm
     template_name = 'registration/change-password.html'
     success_url  = reverse_lazy('password_success')
     # success_url  = reverse_lazy('home')
