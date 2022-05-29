5089b5fd2ca15b13b925be9ec78244765a75a11b
AlienHM <alihesenov15@gmail.comm>
2022-05-29 14:05:45 +0300

    first commit

---
+++ blog/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Post, Category
+
+choices = Category.objects.all().values_list('name', 'name')
+choices_l = []
+for item in choices:
+    choices_l.append(item)
+    
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'author', 'category', 'body')
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Let\'s write some title to your Post'}),
+            'author': forms.TextInput(attrs={'class':'form-control', 'value':'', 'id':'username', 'type':'hidden'}),
+            'category': forms.Select(choices=choices_l, attrs={'class':'form-control'}),
+            'body': forms.Textarea(attrs={'class':'form-control'}),
+        }
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'body')
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Write title for post'}),
+            'body': forms.Textarea(attrs={'class':'form-control'}),
+        }
+
---
+++ blog/models.py
@@ -0,0 +1,29 @@
+
+from django.urls import reverse
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import date, datetime
+# Create your models here.
+class Category (models.Model):
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('home')
+        # return reverse('post_detail', kwargs={"pk": self.pk})
+class Post (models.Model):
+    title=models.CharField(max_length=100)
+    author=models.ForeignKey(User, on_delete=models.CASCADE)
+    body=models.TextField()
+    post_date = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=255, default="coding")
+    likes = models.ManyToManyField(User, related_name='blog_posts')
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author) 
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={"pk": self.pk})
