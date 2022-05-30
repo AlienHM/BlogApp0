2009e125781d9b978558cf8ccd8c9ee9da653122
AlienHM <alihesenov15@gmail.comm>
2022-05-30 03:23:54 +0300

    db changes

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -86,7 +86,13 @@ DATABASES = {
         'PORT': 1200
     }
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'CONN_MAX_AGE': 500
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
