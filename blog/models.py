39a1a24ce4db6f44c25754a74aa4cc80da72d9d6
AlienHM <alihesenov15@gmail.comm>
2022-06-01 16:32:25 +0300

    test0

--- blog/models.py
+++ blog/models.py
@@ -18,17 +18,17 @@ class Category (models.Model):
 class Post (models.Model):
     title=models.CharField(max_length=100)
     author=models.ForeignKey(User, on_delete=models.CASCADE)
-    bodyy=RichTextField(blank=True, null=True)
-    post_date = models.DateField(auto_now_add=True)
-    category = models.CharField(max_length=255, default="coding")
-    snip=models.CharField(max_length=100, default="Click above to read more...")
-    likes = models.ManyToManyField(User, related_name='blog_posts')
+    # body=RichTextField(blank=True, null=True)
+    # post_date = models.DateField(auto_now_add=True)
+    # category = models.CharField(max_length=255, default="coding")
+    # snip=models.CharField(max_length=100, default="Click above to read more...")
+    # likes = models.ManyToManyField(User, related_name='blog_posts')
 
     # body=models.TextField()
     # category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-    def total_likes(self):
-        return self.likes.count()
+    # def total_likes(self):
+    #     return self.likes.count()
 
     def __str__(self):
         return self.title + ' | ' + str(self.author) 
